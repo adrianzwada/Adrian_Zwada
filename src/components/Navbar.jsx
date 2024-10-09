536a5f3acabc0bc41960c65d60e463fbe3f8c269
Adrian Zwada <adrian.zwada@gmail.com>
2024-10-10 01:15:31 +0200

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import logo from '../assets/Logo/logo-no-background-v2.png'
+import logo from '../../assets/Logo/logo-no-background-v2.png'
 import { FaLinkedin } from 'react-icons/fa'
 import { FaGithub } from 'react-icons/fa'
 import { SiCodewars } from 'react-icons/si'
@@ -11,13 +11,13 @@ const NavBar = () => {
 				<img className='w-100 rounded-sm object-cover h-12 mx-2' src={logo} alt='Logo' />
 			</div>
 			<div className='m-8 flex items-center justify-center gap-4 text-2xl '>
-				<a href={CONTACT.linkedin} className='hover:text-cyan-300'>
+				<a href={CONTACT.linkedin} className='hover:text-cyan-300' aria-label='Visit my LinkedIn profile'>
 					<FaLinkedin />
 				</a>
-				<a href={CONTACT.github} className='hover:text-cyan-300'>
+				<a href={CONTACT.github} className='hover:text-cyan-300' aria-label='Visit my Github profile'>
 					<FaGithub />
 				</a>
-				<a href={CONTACT.codewars} className='hover:text-cyan-300'>
+				<a href={CONTACT.codewars} className='hover:text-cyan-300 ' aria-label='Visit my Codewars profile'>
 					<SiCodewars />
 				</a>
 			</div>
