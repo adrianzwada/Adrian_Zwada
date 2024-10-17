70d8001ad500fd811c4f84c4632d7915612735ce
Adrian Zwada <adrian.zwada@gmail.com>
2024-10-17 13:25:52 +0200

    Update Contact.jsx

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,7 +2,7 @@ import React, { useRef, useState } from 'react'
 import emailjs from '@emailjs/browser'
 import { publicKey } from '../constants/config.js'
 import { motion } from 'framer-motion'
-import ReCAPTCHA from 'react-google-recaptcha'
+// import ReCAPTCHA from 'react-google-recaptcha'
 
 const ContactMe = ({ contactRef }) => {
 	const form = useRef()
@@ -65,7 +65,7 @@ const ContactMe = ({ contactRef }) => {
 						className='bg-[#10172d] w-full border rounded-md border-[#353a52] focus:border-cyan-500 ring-0 outline-0 transition-all duration-300 px-3 py-2  text-blue-300'
 						placeholder='Message'
 					></textarea>
-					<ReCAPTCHA sitekey='6LfJg_cpAAAAANTW6kxtbWVSwptMEkgnAiLOdx0f' onChange={val => setCapVal(val)} theme='dark' />
+{/* 					<ReCAPTCHA sitekey='6LfJg_cpAAAAANTW6kxtbWVSwptMEkgnAiLOdx0f' onChange={val => setCapVal(val)} theme='dark' /> */}
 					<button
 						disabled={!capVal}
 						type='submit'
